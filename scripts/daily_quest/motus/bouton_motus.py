b629f023e7f06d079269be823c034336d1d7ae49
Mathieu-ab <mathieu3577aversenc@gmail.com>
2024-02-08 15:43:44 +0100

    update for ubuntu
    test si la commande est faite en DM ne marchais pas sur ubuntu

--- scripts/daily_quest/motus/bouton_motus.py
+++ scripts/daily_quest/motus/bouton_motus.py
@@ -8,25 +8,24 @@ class Motus(discord.ui.View):
     async def demarer_button_callback(self, button, interaction):
         interaction = select_interaction_argument(interaction, button)
         if test_daily_quest_completed(interaction.user.id) == False :
-            if str(interaction.user.id) in motus_msg_player : #true si il a déjà démarer le jeu
+            if str(interaction.user.id) in motus_msg_player and "Direct Message with" in str(interaction.channel): #true si il a déjà démarer le jeu
                 mot_mystere = get_mot_mystere()
                 nb_chance_left = motus_msg_player[f"{interaction.user.id}"]["nb_chance_left"]
                 embed = discord.Embed(title=f"""Tentative{pluriel(nb_chance_left)} restante{pluriel(nb_chance_left)} : {nb_chance_left}
 mot en {len(mot_mystere)} lettres""")
                 name_img = genere_image_motus(mot_mystere, motus_msg_player[f"{interaction.user.id}"]["mot_donnes"])
                 embed.set_image(url=f"attachment://{name_img}.png")
-                msg = await interaction.response.send_message(embed=embed, file=discord.File(CURRENT_PATH+f'/assets/img_tamp/{name_img}.png'), ephemeral=True)
+                await interaction.response.send_message(embed=embed, file=discord.File(CURRENT_PATH+f'/assets/img_tamp/{name_img}.png'), ephemeral=True)
                 motus_msg_player[str(interaction.user.id)]["interaction"] = interaction
-                motus_msg_player[str(interaction.user.id)]["message_motus"] = msg
                 os.remove(CURRENT_PATH+f"/assets/img_tamp/{name_img}.png")
-            elif isinstance(interaction.channel, discord.channel.DMChannel) : #premier message affichant le motus
+            elif "Direct Message with" in str(interaction.channel) : #premier message affichant le motus
                 nb_chance_left = 6
                 mot_mystere = get_mot_mystere()
                 embed = discord.Embed(title=f"""Tentative{pluriel(nb_chance_left)} restante{pluriel(nb_chance_left)} : {nb_chance_left}
 mot en {len(mot_mystere)} lettres""")
                 name_img = genere_FIRST_image_motus(mot_mystere)
                 embed.set_image(url=f"attachment://{name_img}.png")
-                msg = await interaction.response.send_message(embed=embed, file=discord.File(CURRENT_PATH+f'/assets/img_tamp/{name_img}.png'), ephemeral=True)
+                await interaction.response.send_message(embed=embed, file=discord.File(CURRENT_PATH+f'/assets/img_tamp/{name_img}.png'), ephemeral=True)
                 os.remove(CURRENT_PATH+f"/assets/img_tamp/{name_img}.png")
                 motus_msg_player[str(interaction.user.id)] = {"nb_chance_left" : 6, "mot_donnes" : []}
             else :
