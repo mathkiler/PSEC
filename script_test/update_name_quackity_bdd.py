392e84da245177824b237ea369ddc2e1d10356cd
Mathieu-ab <mathieu3577aversenc@gmail.com>
2024-03-18 21:40:45 +0100

    update file resolv bug quaCkity
    le pathing pour accéder à la bdd depuis ce file n'était pas bonne, elle a été résolu mnt

--- script_test/update_name_quackity_bdd.py
+++ script_test/update_name_quackity_bdd.py
@@ -1,6 +1,8 @@
 import sqlite3
+import os
 
-from scripts.global_commandes.import_et_variable import db_path
+CURRENT_PATH = os.getcwd()[:-12]
+db_path = CURRENT_PATH+f'/assets/database/database.db'
 
 
 baseDeDonnees = sqlite3.connect(db_path)
@@ -10,7 +12,6 @@ curseur = baseDeDonnees.cursor()
 
 
     
-
 curseur.execute(f"""UPDATE Cartes 
       SET nom = C_El_Quackity
       WHERE id == 4""")
@@ -19,6 +20,7 @@ curseur.execute(f"""UPDATE Cartes
 curseur.execute(f"""UPDATE Cartes 
       SET nom = R_Quackity
       WHERE id == 7""")
+
 
 
 
